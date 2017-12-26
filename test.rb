1b1a242f3f86f9b810f7eeaa43ad021231da7bc2
duanmengmin <duanmm@1391.com>
2017-12-26 10:35:52 +0800

    update

--- test.rb
+++ test.rb
@@ -2,13 +2,16 @@
 
 
 # xcodemodify使用示例脚本
-# 终端下切到脚本所在目录，执行 'ruby test.rb'即可（请先确保安装了xcodeproj组件）
 
 $LOAD_PATH << '.'
 require 'xcodemodify'
 
+# 设置需要调整的工程路径
 project_path = './test/test.xcodeproj'
+# 设置配置的json文件路径
 mod_path = './xcmod.json'
 xcproject = XcodeModify::XCProject.new(project_path, mod_path)
+# 设置新的调整后的工程路径
 project_path_new = './test/test2.xcodeproj'
+# 传入路径并执行脚本，如若不传则修改原工程
 xcproject.apply_modify(project_path_new)
